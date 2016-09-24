93d056c0bd787294f93dce77c89540c4a34e3616
Anna <verdyanna@gmail.com>
2016-09-24 16:48:42 +0300

    basic template

---
+++ fixture/application.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Irsen'
+
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.session = SessionHelper(self)
+
+        self.base_url = base_url
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+# после Задание №16: (опции командной строки, позволяющие указывать тип браузера и адрес)
+# эта проверка становится лишней
+# if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_id("maintable")) > 0):
+        wd.get(self.base_url)
+
+    def destroy(self):
+        self.wd.quit()
