bfc03caac765f144a5d00945806ff6137298c49d
Anna <verdyanna@gmail.com>
2016-09-25 17:08:14 +0300

    проверка

--- test/test_all.py
+++ test/test_all.py
@@ -6,7 +6,6 @@ from selenium.common.exceptions import NoSuchElementException
 @pytest.fixture()
 def app(request):
     wd = webdriver.Chrome()
-    #wd.get('https://www.addressbook.net/cgi-bin/WebObjects/AddressBook.woa/wa/UserAction/login')
     def destroy():
         wd.close()
     request.addfinalizer(destroy)
@@ -23,9 +22,19 @@ def test_mail_in(app):
         app.find_element_by_id('login').send_keys(row[0].value)
         app.find_element_by_id('password').send_keys(row[1].value)
         app.find_element_by_id('loginSubmit').click()
-
         wb.save(filename="C:\\Reposit\\soft_sob\\data_result.xlsx")
         row_number += 1
+
+def ensure_logout(self):
+    if len(app.find_elements_by_link_text("logout"))>0:
+        self.logout()
+    else:
+        self.registr()
+
+def logout():
+    app.find_element_by_id(".//*[@id='logoutlink").click()
+
+
 
 
 
